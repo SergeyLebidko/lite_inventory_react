3aa1552c3f2330a15a048d16433cdccae07876af
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-17 12:42:22 +0300

    Установлен node-sass. Начата работа над компонентом для входа

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginForm from './LoginForm/LoginForm';
 import './App.css';
 
 function App() {
     return (
         <div>
             Здесь будет мое приложение
+            <LoginForm/>
         </div>
     );
 }
---
+++ src/LoginForm/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './LoginForm.module.scss';
+
+function LoginForm() {
+    return (
+        <div>
+            Здесь будет форма для входа
+        </div>
+    );
+}
+
+export default LoginForm;
