fa8400d486e177be2e85839cc0d6a4c147849ff2
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-18 19:42:32 +0300

    Создан компонент ServiceDescription.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ServiceDescription from './ServiceDescription/ServiceDescription';
 import AccountControl from './AccountControl/AccountControl';
 import './App.css';
 
 function App() {
     return (
         <div>
-            Здесь будет описание сервиса
+            <ServiceDescription/>
             <AccountControl/>
         </div>
     );
--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -20,9 +20,9 @@ function LoginForm({loginError, loadToken}) {
     return (
         <div>
             Для использования сервиса введите учетные данные:
-            Логин: <input type={"text"} value={login} onChange={loginChangeHandler}/>
-            Пароль: <input type={"text"} value={password} onChange={passwordChangeHandler}/>
-            <input type={"button"} value={"Войти"} onClick={() => loadToken(login, password)}/>
+            Логин: <input type="text" value={login} onChange={loginChangeHandler}/>
+            Пароль: <input type="text" value={password} onChange={passwordChangeHandler}/>
+            <input type="button" value="Войти" onClick={() => loadToken(login, password)}/>
             {loginError ? <div>Данные не верны</div> : ''}
         </div>
     );
