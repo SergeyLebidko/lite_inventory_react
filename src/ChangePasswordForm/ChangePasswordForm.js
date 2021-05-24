8a15cc30888bb06198661f1fa0949cd063341864
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-24 13:57:40 +0300

    Создана разметка для ChangePasswordForm.js

--- src/ChangePasswordForm/ChangePasswordForm.js
+++ src/ChangePasswordForm/ChangePasswordForm.js
@@ -8,9 +8,38 @@ import mapDispatchToPropsFactory from '../store/dispatchMaps';
 function ChangePasswordForm({cancelHandler}) {
     return (
         <div className={style.container}>
-            Здесь будет форма для смены пароля
+            <h1>Для смены пароля введите старый пароль, затем новый и его подтверждение</h1>
+            <table>
+                <tbody>
+                <tr>
+                    <td>
+                        <label htmlFor="current_password_field">Текущий пароль:</label>
+                    </td>
+                    <td>
+                        <input type="password" id="current_password_field"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="password1_field">Новый пароль:</label>
+                    </td>
+                    <td>
+                        <input type="password" id="password1_field"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="password2_field">Новый пароль (ещё раз):</label>
+                    </td>
+                    <td>
+                        <input type="password" id="password2_field"/>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
             <div className={style.control}>
                 <input type="button" value="Отмена" onClick={cancelHandler}/>
+                <input type="button" value="Сохранить"/>
             </div>
         </div>
     );
