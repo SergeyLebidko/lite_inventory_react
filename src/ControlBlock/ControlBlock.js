14773b5e705877851e64fab9a061eaeb1b496266
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-28 17:46:34 +0300

    В ControlBlock.js добавлены кнопки

--- src/ControlBlock/ControlBlock.js
+++ src/ControlBlock/ControlBlock.js
@@ -3,9 +3,20 @@ import style from './ControlBlock.module.scss';
 
 function ControlBlock() {
     return (
-        <div className={style.container}>
-            Здесь будет блок управления
-        </div>
+        <>
+            <div className={`${style.container} ${style.group_control}`}>
+                <input type="button" value="Добавить"/>
+                <input type="button" value="Переименовать"/>
+                <input type="button" value="Удалить"/>
+            </div>
+            <div className={`${style.container} ${style.equipment_control}`}>
+                <input type="button" value="На главную"/>
+                <input type="button" value="Добавить"/>
+                <input type="button" value="Редактировать"/>
+                <input type="button" value="Удалить"/>
+                <input type="button" value="Статистика"/>
+            </div>
+        </>
     );
 }
 
