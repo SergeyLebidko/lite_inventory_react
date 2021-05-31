c1af839d9bdac8c5f98af2478458f1a36414898b
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-31 20:44:42 +0300

    Revert "connector внедрен в RenameGroupModal.js"
    This reverts commit 48d7d548

--- src/store/storeConnector.js
+++ src/store/storeConnector.js
@@ -3,7 +3,7 @@ import mapStateToPropsFactory from './stateMaps';
 import mapDispatchToPropsFactory from './dispatchMaps';
 
 export default function connector(component){
-    let statMap = mapStateToPropsFactory(component.name) || {};
-    let dispatchMap = mapDispatchToPropsFactory(component.name) || {};
-    return connect(statMap, dispatchMap)(component);
+    let statMap = mapStateToPropsFactory(component) || {};
+    let dispatchMap = mapDispatchToPropsFactory(component) || {};
+    return connect(statMap, dispatchMap);
 }
