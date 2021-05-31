c8f87016d1c83860e81bffda7590e39639c3c9c7
SergeyLebidko <it.sergeyler@mail.ru>
2021-05-31 20:44:49 +0300

    Revert "Добавлен storeConnector.js"
    This reverts commit 39008069

--- src/store/storeConnector.js
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux';
-import mapStateToPropsFactory from './stateMaps';
-import mapDispatchToPropsFactory from './dispatchMaps';
-
-export default function connector(component){
-    let statMap = mapStateToPropsFactory(component) || {};
-    let dispatchMap = mapDispatchToPropsFactory(component) || {};
-    return connect(statMap, dispatchMap);
-}
