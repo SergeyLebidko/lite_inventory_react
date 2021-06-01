3e30381003076adc4c8262e0ccee76d1e58a9e05
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-01 13:13:38 +0300

    createMaps внедрен в GroupBlock.js

--- src/GroupBlock/GroupBlock.js
+++ src/GroupBlock/GroupBlock.js
@@ -1,10 +1,8 @@
 import React, {useEffect} from 'react';
-import style from './GroupBlock.module.scss';
 import GroupList from '../GroupList/GroupList';
-
 import {connect} from 'react-redux';
-import mapDispatchToPropsFactory from '../store/dispatchMaps';
-import mapStateToPropsFactory from '../store/stateMaps';
+import {createMaps} from '../store/maps';
+import style from './GroupBlock.module.scss';
 
 function GroupBlock(props) {
     let {
@@ -40,6 +38,4 @@ function GroupBlock(props) {
     );
 }
 
-let stateMap = mapStateToPropsFactory('GroupBlock');
-let dispatchMap = mapDispatchToPropsFactory('GroupBlock');
-export default connect(stateMap, dispatchMap)(GroupBlock);
+export default connect(...createMaps('GroupBlock'))(GroupBlock);
